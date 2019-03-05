d29fa939e9828d76a2c5f045fde2a8af91a09bcb
busitora <ts12272530@gmail.com>
2019-03-05 16:05:01 +0900

    showアクションを追加し、routeとviewを追加しました

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,3 +1,25 @@
 class CalcsController < ApplicationController
 
+  def show
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @cal = params[:cal].to_s
 
+    if
+      @cal == "addition"
+      @result = @num1 + @num2
+    elsif
+      @cal== "subtraction"
+      @result = @num1 - @num2
+    elsif
+      @cal == "multiplication"
+      @result = @num1 * @num2
+    elsif
+      @cal == "division"
+      @result = @num1 / @num2
+    else
+      @result = "数値が正しくありません"
+    end
+  end
+
+end
