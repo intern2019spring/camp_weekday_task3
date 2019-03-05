d70aa717d47499240e6ac5ef6c508e342bd7cee4
keigo999 <highbridgek5@gmail.com>
2019-03-06 08:41:29 +0900

    コントローラとアクション実装

---
+++ app/controllers/calcs_controller.rb
@@ -0,0 +1,19 @@
+class CalcsController < ApplicationController
+  def show
+    @price1 = params[:price1].to_i
+    @price2 = params[:price2].to_i
+    @oprator = params[:oprator]
+    
+    def total
+    case @oprator
+    when "addition"
+      @total = @price1 + @price2
+    when "subtraction"
+      @total = @price1 - @price2
+    when "multiplication"
+      @total = @price1 * @price2
+    when "division"
+      @total = @price1 / @price2s
+    end
+  end
+end
