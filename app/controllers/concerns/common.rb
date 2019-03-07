8038e2da76e551ccbc38b1badc19799e40965635
busitora <ts12272530@gmail.com>
2019-03-07 18:35:21 +0900

    インデント修正

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -6,20 +6,21 @@ module Common
     num2 = params[:num2].to_i
     @cal = params[:cal]
 
-    @result = if @cal == "addition"
-      num1 + num2
-    elsif @cal == "subtraction"
-      num1 - num2
-    elsif @cal == "multiplication"
-      num1 * num2
-    elsif @cal == "division"
-      begin
-      "#{num1 / num2}".."#{num1 % num2}"
-      rescue ZeroDivisionError
-      "divided by 0"
+    @result = 
+      if @cal == "addition"
+        num1 + num2
+      elsif @cal == "subtraction"
+        num1 - num2
+      elsif @cal == "multiplication"
+        num1 * num2
+      elsif @cal == "division"
+        begin
+        "#{num1 / num2}".."#{num1 % num2}"
+        rescue ZeroDivisionError
+        "divided by 0"
+        end
+      else
+        "URLが正しくありません"
       end
-    else
-      "URLが正しくありません"
-    end
   end
 end
